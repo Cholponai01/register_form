10701f597e9813c6973b87d34efca20c0a007117
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2023-12-11 23:46:46 +0600

    upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:register_form/config/config.dart';
-import 'package:register_form/features/auth/presentation/page/registration/sign_in_page.dart';
 import 'package:register_form/features/auth/presentation/page/registration/sign_up_page.dart';
 
 void main() {
